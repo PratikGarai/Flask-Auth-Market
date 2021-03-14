ca7a27022788a4813767da56efad1f296885d6a7
PratikGarai <pratikgarai0208@gmail.com>
2021-03-14 08:04:18 +0530

    basic market page

--- market.py
+++ market.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask import render_template
+from datas import items
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template("home.html")
+
+
+@app.route('/market')
+def market_page():
+    context = {
+        "items" : items
+    }
+
+    return render_template("market.html", **context)
