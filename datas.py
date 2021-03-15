6f3ad143e9bcf6fc12db1e153b682e37a3cf1175
PratikGarai <pratikgarai0208@gmail.com>
2021-03-15 09:30:04 +0530

    db configurations

--- datas.py
+++ datas.py
@@ -1,5 +1,15 @@
 items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500, 'description':"An ordinary phone with a lot of extraordinary features"},
+    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900, 'description':"An ordinary laptop with a lot of extraordinary features"},
+    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150, 'description':"An ordinary keyboard with a lot of extraordinary features"}
 ]
+
+from market import Item, db
+
+def generate_Items():
+    for it in items :
+        item = Item(name=it['name'], barcode=it['barcode'], price=it['price'], description=it['description'])
+        db.session.add(item)
+        db.session.commit()
+
+    print(Item.query.all())
--- market.py
+++ market.py
@@ -1,8 +1,18 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from datas import items
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app=app)
+
+class Item(db.Model) :
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False)
 
 
 @app.route('/')
