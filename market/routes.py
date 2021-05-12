db48eb0760dd487eead99de5f7c36673bbbd6272
PratikGarai <pratikgarai0208@gmail.com>
2021-05-12 20:46:03 +0530

    more stuff to auth

--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from market.models import Item, User
 from market.forms import LoginForm, RegisterForm
 from market import db
 from flask.helpers import flash
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route('/market')
+@login_required
 def market_page():
     context = {
         "items" : Item.query.all()
@@ -33,7 +34,9 @@ def register_page():
                             email = form.email.data,
                             password = form.password1.data)
         db.session.add(user_to_create)
+        flash(f'Success! You are logged in as {user_to_create.username}', 'success')
         db.session.commit()
+        login_user(user_to_create)
         return redirect(url_for('market_page'))
 
     if form.errors != {} :
@@ -71,6 +74,7 @@ def login_page():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('home_page'))
